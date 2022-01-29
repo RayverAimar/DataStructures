f47a58619022a776cef1f99c0c5fc48bb40ffb0c
Rayver Aimar <rayver.munoz@ucsp.edu.pe>
2022-01-28 22:06:01 -0500

    top & empty functions implemented

--- Stack/stack.cpp
+++ Stack/stack.cpp
@@ -8,8 +8,9 @@ struct CNode{
     CNode<T>* m_prev;
     CNode(T _v, CNode<T>* n = nullptr);
 };
+
 template<class T>
-CNode<T>::CNode(T _v, CNode<T>* n = nullptr){
+CNode<T>::CNode(T _v, CNode<T>* n){
     m_v = _v;
     m_prev = n;
 }
@@ -18,7 +19,17 @@ template<class T>
 struct CStack{
     CNode<T>* m_top;
     CStack();
+
+
     
+    T top();
+    void push();
+    void pop();
+    void print();
+    
+    int size();
+    bool empty();
+
 };
 
 template<class T>
@@ -26,8 +37,36 @@ CStack<T>::CStack(){
     m_top = nullptr;
 }
 
+template<class T>
+T CStack<T>::top(){
+    return m_top->m_v;
+}
 
+template<class T>
+void CStack<T>::push(){
+    
+}
+
+template<class T>
+void CStack<T>::pop(){
+
+}
+
+template<class T>
+void CStack<T>::print(){
+    
+}
+
+template<class T>
+int CStack<T>::size(){
+    return 0;
+}
+
+template<class T>
+bool CStack<T>::empty(){
+    return (!m_top) ? true : false;
+}
 
 int main(){
-
+    
 }
